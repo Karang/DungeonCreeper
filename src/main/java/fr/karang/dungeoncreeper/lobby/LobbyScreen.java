04e80b2ff5db2d6be9894b704baeab291c94e2b6
Karang <arthur.hennequin@free.fr>
2012-11-16 18:46:17 +0100

    More gui for the lobby

--- src/main/java/fr/karang/dungeoncreeper/lobby/LobbyScreen.java
+++ src/main/java/fr/karang/dungeoncreeper/lobby/LobbyScreen.java
@@ -26,6 +26,7 @@
  */
 package fr.karang.dungeoncreeper.lobby;
 
+import java.awt.Color;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -35,16 +36,25 @@ import org.spout.api.chat.style.ChatStyle;
 import org.spout.api.gui.Screen;
 import org.spout.api.gui.Widget;
 import org.spout.api.gui.component.LabelComponent;
+import org.spout.api.gui.component.RenderPartsHolderComponent;
+import org.spout.api.gui.render.RenderPart;
 import org.spout.api.math.Rectangle;
 import org.spout.api.plugin.Platform;
 import org.spout.api.render.Font;
+import org.spout.api.render.RenderMaterial;
 
 import fr.karang.dungeoncreeper.DungeonCreeper;
 
 public class LobbyScreen extends Screen {
+	private static final float SCALE = 0.75f; // TODO: Apply directly from engine
+	private static final Color GUI_BROWN = new Color(110, 70, 0, 200);
+	private static final Color GUI_GREEN = new Color(100, 160, 0, 200);
+	//private static final Color GUI_GREY = new Color(128, 128, 128, 200);
+	private final RenderMaterial colorMaterial = (RenderMaterial) Spout.getFilesystem().getResource("material://Spout/resources/resources/materials/GUIColorMaterial.smt");
 	private final Font FONT = (Font) Spout.getFilesystem().getResource("font://DungeonCreeper/resources/gui/DKFont.ttf");
 	private Widget players = new Widget();
 	private List<Widget> games = new ArrayList<Widget>();
+	private Widget[][] gamesTab = new Widget[5][3];
 	private float gameListOffset = 0.9f;
 	
 	public LobbyScreen(Lobby lobby) {
@@ -54,11 +64,39 @@ public class LobbyScreen extends Screen {
 		
 		this.setTakesInput(false);
 		
+		newTextBox(new ChatArguments("Dungeon's name"), GUI_BROWN, -0.95f, 0.5f, 0.3f, 0.05f);
+		newTextBox(new ChatArguments("Online players"), GUI_BROWN, -0.64f, 0.5f, 0.3f, 0.05f);
+		newTextBox(new ChatArguments("Maximum players"), GUI_BROWN, -0.33f, 0.5f, 0.3f, 0.05f);
+		
+		for (int x=0 ; x<3 ; x++) {
+			for (int y=0 ; y<5 ; y++) {
+				Widget box = newTextBox(new ChatArguments(""), GUI_GREEN, -0.95f+0.31f*x, 0.44f-0.06f*y, 0.3f, 0.05f);
+				gamesTab[y][x] = box;
+			}
+		}
+		
 		players.setGeometry(new Rectangle(0,0,0,0));
 		LabelComponent playerList = players.add(LabelComponent.class);
 		playerList.setFont(FONT);
 		playerList.setText(new ChatArguments(ChatStyle.YELLOW, "Online players: "));
 		attachWidget(DungeonCreeper.getInstance(), players);
+	}
+	
+	public Widget newTextBox(ChatArguments label, Color bg, float x, float y, float w, float h) {
+		final Widget box = new Widget();
+		final RenderPartsHolderComponent titlerect = box.add(RenderPartsHolderComponent.class);
+		RenderPart rect = new RenderPart();
+		rect.setRenderMaterial(colorMaterial);
+		rect.setColor(bg);
+		rect.setSprite(new Rectangle(x*SCALE, y, w*SCALE, h));
+		rect.setSource(new Rectangle(0, 0, 0, 0));
+		titlerect.add(rect);
+		box.setGeometry(new Rectangle(x*SCALE, y, 0, 0));
+		final LabelComponent title = box.add(LabelComponent.class);
+		title.setFont(FONT);
+		title.setText(label);
+		attachWidget(DungeonCreeper.getInstance(), box);
+		return box;
 	}
 	
 	public void addPlayer(String name) {
